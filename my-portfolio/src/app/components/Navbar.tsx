c84cf3a50f38a75e975962a02d31caf23c209b11
sue-warbuzz <59034961+sue-warbuzz@users.noreply.github.com>
2025-05-15 20:30:53 -0400

    new changes

--- my-portfolio/src/app/components/Navbar.tsx
+++ my-portfolio/src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const navItems = [
   { href: '/about', label: 'About' },
   { href: '/projects', label: 'Projects' },
   { href: '/resume', label: 'Resume' },
-  { href: '/contact', label: 'Contact' },
+  { href: 'https://www.linkedin.com/in/sudipti-sharma-8343b618b', label: 'LinkedIn'},
   { href: '/blog', label: 'Blog' },
 ]
 
